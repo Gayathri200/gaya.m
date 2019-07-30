4fc51280f44327f1cfc79e1b0bb39ee415f82cc8
gayathri.m <53171194+Gayathri200@users.noreply.github.com>
2019-07-30 17:52:26 +0530

    Create holi.py

---
+++ holi.py
@@ -0,0 +1,15 @@
+a=str(input())
+if(a=="Sunday"):
+  print("yes")
+elif(a=="Saturday"):
+  print("yes")  
+elif(a=="Monday"):
+  print("no")   
+elif(a=="Tuesday"):
+  print("no")
+elif(a=="Wednesday"):
+  print("no")
+elif(a=="Thursday"):
+  print("no")   
+elif(a=="Friday"):
+  print("no")   
